6f2762295e636a21652178f4bb91d1a5654817f5
Amir Pasha Zarghami <apashazarghami@gmail.com>
2022-09-19 07:32:17 -0700

    set month of year

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,7 @@
 const myDate = new Date();
+document.querySelector(".day").innerText = setDay();
+document.querySelector(".date").innerText = `${myDate.getDate()}th`;
+document.querySelector(".month").innerText = setMonth();
 
 function setDay() {
     const dayOfWeek = myDate.getDay();
@@ -6,17 +9,48 @@ function setDay() {
     if(dayOfWeek === 1) {
         day = "Monday";
     } else if(dayOfWeek === 2) {
-        day = "Tuesday"
+        day = "Tuesday";
     } else if(dayOfWeek === 3) {
-        day = "Wednesday"
+        day = "Wednesday";
     } else if(dayOfWeek === 4) {
-        day = "Thursday"
+        day = "Thursday";
     } else if(dayOfWeek === 5) {
-        day = "Friday"
+        day = "Friday";
     } else if(dayOfWeek === 6) {
-        day = "Saturday"
+        day = "Saturday";
     } else if(dayOfWeek === 7) {
-        day = "Sunday"
+        day = "Sunday";
     }
     return day;
+}
+
+function setMonth() {
+    const monthOfYear = myDate.getMonth();
+    let month;
+    if(monthOfYear === 0) {
+        month = "January"; 
+    } else if(monthOfYear === 1) {
+        month = "February";
+    } else if(monthOfYear === 2) {
+        month = "March";
+    } else if(monthOfYear === 3) {
+        month = "April";
+    } else if(monthOfYear === 4) {
+        month = "May";
+    } else if(monthOfYear === 5) {
+        month = "June";
+    } else if(monthOfYear === 6) {
+        month = "July";
+    } else if(monthOfYear === 7) {
+        month = "August";
+    } else if(monthOfYear === 8) {
+        month = "September";
+    } else if(monthOfYear === 9) {
+        month = "October";
+    } else if(monthOfYear === 10) {
+        month = "November";
+    } else if(monthOfYear === 11) {
+        month = "December";
+    }
+    return month;
 }
